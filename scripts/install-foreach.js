8dcc10235d88e00473787bf23192d1417a2dc939
OrangeJ <juhaotian@foxmail.com>
2018-08-03 00:34:39 +0800

    TODO: scripts/foreach.js的实现

--- scripts/install-foreach.js
+++ scripts/install-foreach.js
@@ -1,3 +1,8 @@
 'use strict';
 
 console.log('install-foreach..');
+
+// TODO: 为package.json中添加相应字段
+// 1. "scripts": {"pce-foreach": "node ./node_modules/pre-commit-enhanced/scripts/foreach.js"}
+// 2. "pre-commit": ["pce-foreach"]
+// 3. "pce-foreach-command": "command <filename>"
