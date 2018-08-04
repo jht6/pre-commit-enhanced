2d296377acb9f34693a937c3e064391dceb029f3
OrangeJ <juhaotian@foxmail.com>
2018-08-04 22:08:58 +0800

    将单元测试文件放入test目录

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const LONG_TIMEOUT = 10000;
 const fs = require('fs');
 const path = require('path');
 const assume = require('assume');
-const Hook = require('./index');
+const Hook = require('../index');
 
 /* istanbul ignore next */
 describe('pre-commit', () => {
