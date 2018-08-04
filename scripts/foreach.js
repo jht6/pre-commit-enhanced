c897b22cfba2e1f07c5ef8c8a57581eb9550a6ae
HaoTian Ju <412915040@qq.com>
2018-08-04 14:57:58 +0800

    foreach.js中增加获取commit的文件路径列表的伪代码

--- scripts/foreach.js
+++ scripts/foreach.js
@@ -4,5 +4,7 @@ console.log('test foreach');
 
 // TODO: 遍历执行命令
 // 1. 获取到要commit的文件路径列表
+//    使用cross-spawn, 调用spawnSync('git', ['status', '--porcelain'], {stdio: 'pipe'});
+//    获取当前变化的文件, 要过滤出前缀为A或M的项
 // 2. 从package.json中的"pce-foreach-command"解析出要执行的命令
 // 3. 对文件路径列表遍历执行命令
