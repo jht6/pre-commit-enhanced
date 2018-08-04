514c4a32cd75b52b7c628bc3e74f0a490e5c0bc6
HaoTian Ju <412915040@qq.com>
2018-08-04 17:16:18 +0800

    foreach.js的具体实现思路

--- scripts/foreach.js
+++ scripts/foreach.js
@@ -6,5 +6,14 @@ console.log('test foreach');
 // 1. 获取到要commit的文件路径列表
 //    使用cross-spawn, 调用spawnSync('git', ['status', '--porcelain'], {stdio: 'pipe'});
 //    获取当前变化的文件, 要过滤出前缀为A或M的项
+//    并将每项转为绝对路径, 并考虑路径中含空格的情况
 // 2. 从package.json中的"pce-foreach-command"解析出要执行的命令
+//    "cmd-name cmd-opt <filepath>"
+//                   ↓
+//    "cmd-name",  ["cmd-opt", "<filepath>"]
+//                   ↓
 // 3. 对文件路径列表遍历执行命令
+//    "cmd-name",  ["cmd-opt", "绝对路径"]
+//                   ↓
+//    spawnSync("cmd-name", ["cmd-opt", "绝对路径"], {stdio: [0, 1, 2]})
+
