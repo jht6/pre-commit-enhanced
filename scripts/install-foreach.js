926c3fa67f23104e2484866e3b12be7c9a02e621
HaoTian Ju <412915040@qq.com>
2018-08-05 18:09:47 +0800

    install-foreach重构为面向对象实现, 便于后面单元测试

--- scripts/install-foreach.js
+++ scripts/install-foreach.js
@@ -6,81 +6,106 @@ const exists = fs.existsSync || path.existsSync;
 const utils = require('../common/utils');
 const SCRIPT_PCE_FOREACH = 'pce-foreach';
 const { FOREACH_COMMAND_TPL } = require('../common/const')();
-
-let packageJsonPath = path.join(utils.getPackageJsonDirPath(), 'package.json');
-if (!exists(packageJsonPath)) {
-    console.error('pre-commit:');
-    console.error('pre-commit: There is no "package.json" file in path' + packageJsonPath);
-    console.error('pre-commit:');
-    return;
-}
-
-//
-// add "pce-foreach" in "scripts" of package.json
-//
-let json = require(packageJsonPath);
-if (!json) {
-    json = {};
-}
-if (!json.scripts) {
-    json.scripts = {};
-}
-
-json.scripts[SCRIPT_PCE_FOREACH] = [
-    'node ',
-    './node_modules/pre-commit-enhanced/scripts/foreach.js'
-].join('');
-
-//
-// add "pce-foreach" in "pre-commit" of package.json
-//
-let preCommitKey,
-    preCommit;
-if (json['pre-commit']) {
-    preCommitKey = 'pre-commit';
-    preCommit = json[preCommitKey];
-} else if (json['precommit']) {
-    preCommitKey = 'precommit';
-    preCommit = json[preCommitKey];
-} else {
-    preCommitKey = 'pre-commit';
-    preCommit = null;
-}
-
-if (!preCommit) { // no "pre-commit" or "precommit" config now
-    json[preCommitKey] = [SCRIPT_PCE_FOREACH];
-} else if (typeof preCommit === 'object') {
-    if (Array.isArray(json[preCommitKey].run)) { // for format of {run: ["a", "b", "c"]}
-        json[preCommitKey].run.push(SCRIPT_PCE_FOREACH);
-    } else if (typeof json[preCommitKey].run === 'string') { // for format of {run: "a, b, c"}
-        json[preCommitKey].run = preCommit.split(/[, ]+/).push(SCRIPT_PCE_FOREACH).join(', ');
-    } else {
-        json[preCommitKey].run = [SCRIPT_PCE_FOREACH];
-    }
-} else if (typeof preCommit === 'string') { // for format of "a, b, c"
-    json[preCommitKey] = preCommit.split(/[, ]+/).push(SCRIPT_PCE_FOREACH).join(', ');
-} else if (Array.isArray(preCommit)) { // for format of ["a, b, c"]
-    json[preCommitKey].push(SCRIPT_PCE_FOREACH);
-}
-
-//
-// add "pce-foreach-command" property in package.json
-//
 const FOREACH_COMMAND_KEY = 'pce-foreach-command';
-json[FOREACH_COMMAND_KEY] = FOREACH_COMMAND_TPL;
 
+function ForeachInstaller() {
+    if (!this) {
+        return new ForeachInstaller();
+    }
 
-const spaceCount = 2;
-try {
-    fs.writeFileSync(packageJsonPath, JSON.stringify(json, null, spaceCount));
-    console.error('pre-commit:');
-    console.error(`pre-commit: Success: Add "${SCRIPT_PCE_FOREACH}" in "scripts" of package.json`);
-    console.error(`pre-commit: Success: Add "${SCRIPT_PCE_FOREACH}" in "${preCommitKey}" of package.json`);
-    console.error(`pre-commit: Success: Add "${FOREACH_COMMAND_KEY}" in package.json`);
-    console.error(`pre-commit:            at ${packageJsonPath}`);
-    console.error('pre-commit:');
-} catch (e) {
-    console.error('pre-commit:');
-    console.error(`pre-commit: Fail: Add property related to "foreach" in package.json at ${packageJsonPath}`);
-    console.error('pre-commit:');
+    this.packageJsonPath = path.join(
+        utils.getPackageJsonDirPath(),
+        'package.json'
+    );
+    if (!exists(this.packageJsonPath)) {
+        utils.log(`There is no "package.json" file in path: ${this.packageJsonPath}`);
+    }
+
+    this.json = require(this.packageJsonPath);
+    if (!this.json) {
+        this.json = {};
+    }
+    if (!this.json.scripts) {
+        this.json.scripts = {};
+    }
 }
+
+ForeachInstaller.prototype.run = function () {
+    this.json = this.addForeachInScripts(this.json);
+    this.json = this.addForeachInPrecommit(this.json);
+    this.json = this.addForeachCommand(this.json);
+    this.json = this.addForeachCommand(this.json);
+    this.writeJsonToFile(this.json);
+};
+
+ForeachInstaller.prototype.addForeachInScripts = function (json) {
+    json.scripts[SCRIPT_PCE_FOREACH] = [
+        'node ',
+        './node_modules/pre-commit-enhanced/scripts/foreach.js'
+    ].join('');
+
+    return json;
+};
+
+ForeachInstaller.prototype.addForeachInPrecommit = function (json) {
+    let preCommitKey,
+        preCommit;
+    if (json['pre-commit']) {
+        preCommitKey = 'pre-commit';
+        preCommit = json[preCommitKey];
+    } else if (json['precommit']) {
+        preCommitKey = 'precommit';
+        preCommit = json[preCommitKey];
+    } else {
+        preCommitKey = 'pre-commit';
+        preCommit = null;
+    }
+
+    if (!preCommit) { // no "pre-commit" or "precommit" config now
+        json[preCommitKey] = [SCRIPT_PCE_FOREACH];
+    } else if (typeof preCommit === 'object') {
+        if (Array.isArray(json[preCommitKey].run)) { // for format of {run: ["a", "b", "c"]}
+            json[preCommitKey].run.push(SCRIPT_PCE_FOREACH);
+        } else if (typeof json[preCommitKey].run === 'string') { // for format of {run: "a, b, c"}
+            json[preCommitKey].run = preCommit.split(/[, ]+/).push(SCRIPT_PCE_FOREACH).join(', ');
+        } else {
+            json[preCommitKey].run = [SCRIPT_PCE_FOREACH];
+        }
+    } else if (typeof preCommit === 'string') { // for format of "a, b, c"
+        json[preCommitKey] = preCommit.split(/[, ]+/).push(SCRIPT_PCE_FOREACH).join(', ');
+    } else if (Array.isArray(preCommit)) { // for format of ["a, b, c"]
+        json[preCommitKey].push(SCRIPT_PCE_FOREACH);
+    }
+
+    return json;
+};
+
+ForeachInstaller.prototype.addForeachCommand = function (json) {
+
+    json[FOREACH_COMMAND_KEY] = FOREACH_COMMAND_TPL;
+
+    return json;
+};
+
+ForeachInstaller.prototype.writeJsonToFile = function (json) {
+    const spaceCount = 2;
+    try {
+        fs.writeFileSync(this.packageJsonPath, JSON.stringify(json, null, spaceCount));
+        utils.log([
+            `Success: Add "${SCRIPT_PCE_FOREACH}" in "scripts" of package.json`,
+            `Success: Add "${SCRIPT_PCE_FOREACH}" in "pre-commit" of package.json`,
+            `Success: Add "${FOREACH_COMMAND_KEY}" in package.json`,
+            `    at ${this.packageJsonPath}`
+        ]);
+    } catch (e) {
+        utils.log([
+            `Fail: Add property related to "foreach" in package.json at ${this.packageJsonPath}`,
+            `Error message is:`
+        ]);
+        console.log(e);
+    }
+};
+
+let installer = new ForeachInstaller();
+
+installer.run();
