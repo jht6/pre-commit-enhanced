ae46c95d9b55d454e5b817117c9ed123085477e0
OrangeJ <juhaotian@foxmail.com>
2018-08-08 00:23:27 +0800

    foreach: getCommandFromPackageJson()实现

--- scripts/foreach.js
+++ scripts/foreach.js
@@ -5,6 +5,9 @@ console.log('start run foreach');
 const spawn = require('cross-spawn');
 const utils = require('../common/utils');
 const exists = require('fs').existsSync;
+const {
+    FOREACH_COMMAND_KEY
+} = require('../common/const')();
 
 // TODO: 遍历执行命令
 // 1. 获取到要commit的文件路径列表
@@ -28,6 +31,7 @@ function ForeachRunner() {
 
     this.filePathList = [];
     this.gitRootDirPath = process.cwd();
+    this.command = '';
 }
 
 ForeachRunner.prototype.run = function () {
@@ -40,7 +44,15 @@ ForeachRunner.prototype.run = function () {
     }
 
     this.filePathList = this.getFilePathList(gitStatus);
+    if (!this.filePathList.length) {
+        utils.log([
+            `There is nothing to traverse,`,
+            `Skipping running foreach.`
+        ], 0);
+    }
 
+    this.command = this.getCommandFromPackageJson();
+    console.log('this.command: ' + this.command);
 };
 
 ForeachRunner.prototype.getGitStatus = function () {
@@ -73,3 +85,37 @@ ForeachRunner.prototype.getFilePathList = function (gitStatusStr) {
 
     return list;
 };
+
+ForeachRunner.prototype.getCommandFromPackageJson = function () {
+    let json = null,
+        command = '';
+    try {
+        json = require(path.join(
+            utils.getPackageJsonDirPath(),
+            'package.json'
+        ));
+    } catch (e) {
+        utils.log([
+            `Fail: Require json from package.json at ${this.packageJsonPath}`,
+            `Skipping the hook, process will exit..`,
+            `Error message is:`
+        ]);
+        console.log(e);
+        process.exit(0);
+    }
+
+    if (json && json[FOREACH_COMMAND_KEY]) {
+        command = json[FOREACH_COMMAND_KEY];
+    }
+
+    return command;
+};
+
+// Expose the Hook instance so we can use it for testing purposes.
+module.exports = ForeachRunner;
+
+// Run only if this script is executed through CLI
+if (require.main === module) {
+    const foreachRunner = new ForeachRunner();
+    foreachRunner.run();
+}
