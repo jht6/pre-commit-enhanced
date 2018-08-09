bc42e0a8cb1226721914025ef8bcff5d8136797e
HaoTian Ju <412915040@qq.com>
2018-08-09 20:58:20 +0800

    将"<filepath>"放入公共变量FOREACH_COMMAND_PARAM

--- common/const.js
+++ common/const.js
@@ -1,5 +1,8 @@
+const FOREACH_COMMAND_PARAM = '<filepath>';
+
 module.exports = () => ({
     FOREACH_COMMAND_KEY: 'pce-foreach-command',
-    FOREACH_COMMAND_TPL: 'command-name <filepath>',
+    FOREACH_COMMAND_TPL: `command-name ${FOREACH_COMMAND_PARAM}`,
+    FOREACH_COMMAND_PARAM,
     LOG_PREFIX: 'pre-commit:'
 });
