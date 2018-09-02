197a6a77558d0c75fd7433aa48e2769a526037c8
OrangeJ <juhaotian@foxmail.com>
2018-09-03 00:04:27 +0800

    test: getGitStatus和gitFilePathList的单测迁移

--- test/foreach.test.js
+++ test/foreach.test.js
@@ -16,61 +16,6 @@ describe('foreach', function () {
         assume(runner.run).is.a('function');
     });
 
-    describe('#getFilePathList', function () {
-        describe('can correctly execute the first filtering', function () {
-            let runner;
-
-            beforeEach(function () {
-                runner = new ForeachRunner({
-                    isTesting: true,
-                    skipMapToAbsPath: true,
-                    skipFilterNotExist: true
-                });
-            });
-
-            it('can correctly filter empty string', function () {
-                const gitStatus = [
-                    `MM a.js\n`,
-                    `A  b.js\n`,
-                    `?? c.js\n`,
-                    ` A d.js\n`,
-                    ` D e.js\n`
-                ].join('');
-
-                const list = runner.getFilePathList(gitStatus);
-                assume(list.length).is.above(0);
-
-                const emptyList = list.filter(item => !item);
-                assume(emptyList.length).equals(0);
-            });
-
-            it('can correctly filter string starts with "??"', function () {
-                let gitStatus = `?? a.js\n?? b.js\n`;
-                let list = runner.getFilePathList(gitStatus);
-                assume(list.length).equals(0);
-
-                gitStatus = `M  a.js\n?? b.js\n`;
-                list = runner.getFilePathList(gitStatus);
-                assume(list.length).equals(1);
-                assume(list[0]).equals('M  a.js');
-            });
-        });
-
-        it('can correctly map path to absolute path', function () {
-            const gitRoot = '/test/gitroot/';
-            const runner = new ForeachRunner({
-                isTesting: true,
-                gitRoot,
-                skipFilterNotExist: true
-            });
-            const gitStatus = 'M  a.js\n A b.js\n';
-            const list = runner.getFilePathList(gitStatus);
-            assume(list.length).equals(2);
-            assume(list[0]).equals(path.join(gitRoot, `a.js`));
-            assume(list[1]).equals(path.join(gitRoot, `b.js`));
-        });
-    });
-
     describe('#validateCommand & #parseCommand', function () {
         let runner;
 
--- test/utils.test.js
+++ test/utils.test.js
@@ -69,4 +69,55 @@ describe('common/utils', function () {
             assume(ret.lines[0].indexOf('\u001b')).equals(-1);
         });
     });
+
+    describe('#getFilePathList', function () {
+        describe('can correctly execute the first filtering', function () {
+            const testFlag = {
+                isTesting: true,
+                skipMapToAbsPath: true,
+                skipFilterNotExist: true
+            };
+
+            it('can correctly filter empty string', function () {
+                const gitStatus = [
+                    `MM a.js\n`,
+                    `A  b.js\n`,
+                    `?? c.js\n`,
+                    ` A d.js\n`,
+                    ` D e.js\n`
+                ].join('');
+
+                const list = utils.getFilePathList(gitStatus, testFlag);
+                assume(list.length).is.above(0);
+
+                const emptyList = list.filter(item => !item);
+                assume(emptyList.length).equals(0);
+            });
+
+            it('can correctly filter string starts with "??"', function () {
+                let gitStatus = `?? a.js\n?? b.js\n`;
+                let list = utils.getFilePathList(gitStatus, testFlag);
+                assume(list.length).equals(0);
+
+                gitStatus = `M  a.js\n?? b.js\n`;
+                list = utils.getFilePathList(gitStatus, testFlag);
+                assume(list.length).equals(1);
+                assume(list[0]).equals('M  a.js');
+            });
+        });
+
+        it('can correctly map path to absolute path', function () {
+            const gitRoot = '/test/gitroot/';
+            const testFlag = {
+                isTesting: true,
+                gitRoot,
+                skipFilterNotExist: true
+            };
+            const gitStatus = 'M  a.js\n A b.js\n';
+            const list = utils.getFilePathList(gitStatus, testFlag);
+            assume(list.length).equals(2);
+            assume(list[0]).equals(path.join(gitRoot, `a.js`));
+            assume(list[1]).equals(path.join(gitRoot, `b.js`));
+        });
+    });
 });
