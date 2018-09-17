776aef334ebd2cd6eff0f6aeb9378f1b999be6ec
OrangeJ <juhaotian@foxmail.com>
2018-09-18 01:09:56 +0800

    fix: getGitStatus中取git根目录时起点改为由本文件向上跳四级文件夹

--- common/utils/getGitStatus.js
+++ common/utils/getGitStatus.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const spawn = require('cross-spawn');
 
 const log = require('./log');
@@ -6,14 +7,22 @@ const getGitRootDirPath = require('./getGitRootDirPath');
 /**
  * Execute "git status --porcelain" in Git project root folder,
  * and get a status string that can be parsed simply.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *                     IMPORTANT
+ *  This function depends on this file's position.
+ *  If this file is move, notice to check the code.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
  * @return {String} string of git status
  */
 function getGitStatus() {
     let status = '';
+    let startPath = path.resolve(__dirname, '../../../..');
     try {
         status = spawn.sync('git', ['status', '--porcelain'], {
             stdio: 'pipe',
-            cwd: getGitRootDirPath(process.cwd())
+            cwd: getGitRootDirPath(startPath)
         }).stdout.toString();
 
         return status;
