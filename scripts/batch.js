688dcca3847b604f24ad596ee5c97e6a7c1e74a3
OrangeJ <juhaotian@foxmail.com>
2018-09-18 01:16:09 +0800

    feat: batch.js 暂时提交

--- scripts/batch.js
+++ scripts/batch.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('++++++++++++ batch is called....++++++++++++');
+const {
+    getGitStatus,
+    getFilePathList,
+    getPackageJsonDirPath
+} = require('../common/utils/index');
+
+const PKG_JSON_DIR_PATH = getPackageJsonDirPath();
+
+let pathList = getFilePathList(getGitStatus());
+
+pathList = pathList
+    .filter(item => /\.js$/.test(item)) // only remain "*.js" path
+    .map(
+        item => item
+            .replace(PKG_JSON_DIR_PATH, '.')
+            .replace(/\\\\/g, '/')
+    );
+
+console.log(`+++++++++++++++++++++++++++++++++`);
+console.log(pathList);
+console.log(`+++++++++++++++++++++++++++++++++`);
