0e6312f428cf3899944cc1c587cdf66a9854cade
OrangeJ <juhaotian@foxmail.com>
2018-09-19 02:06:44 +0800

    feat: batch.js功能与batch-callback.js的简易版

--- scripts/batch.js
+++ scripts/batch.js
@@ -1,23 +1,56 @@
 'use strict';
 
+const { execSync } = require('child_process');
+
+const callbacks = require('./batch-callback');
 const {
     getGitStatus,
     getFilePathList,
-    getPackageJsonDirPath
+    getPackageJsonDirPath,
+    transPathWinToUnix,
+    log
 } = require('../common/utils/index');
 
-const PKG_JSON_DIR_PATH = getPackageJsonDirPath();
+const PKG_JSON_DIR_PATH_UNIX = transPathWinToUnix(getPackageJsonDirPath());
 
 let pathList = getFilePathList(getGitStatus());
 
-pathList = pathList
-    .filter(item => /\.js$/.test(item)) // only remain "*.js" path
-    .map(
-        item => item
-            .replace(PKG_JSON_DIR_PATH, '.')
-            .replace(/\\\\/g, '/')
-    );
+if (!pathList.length) {
+    log('There is no file to be commited, skip hook.', 0);
+}
 
-console.log(`+++++++++++++++++++++++++++++++++`);
-console.log(pathList);
-console.log(`+++++++++++++++++++++++++++++++++`);
+// Transform all paths to Unix format.
+// For example: 'C:\\a\\b' -> '/C/a/b'.
+pathList = pathList.map(transPathWinToUnix);
+
+// Filter paths by "filter" callback function.
+if (typeof callbacks.filter === 'function') {
+    pathList = pathList.filter(callbacks.filter);
+}
+
+if (!pathList.length) {
+    log('There is no file path after filtering, skip hook.', 0);
+}
+
+// If useRelativePath() return true, transform absolute paths to relative paths.
+if (typeof callbacks.useRelativePath === 'function') {
+    if (callbacks.useRelativePath()) {
+        pathList = pathList.map(
+            item => item.replace(PKG_JSON_DIR_PATH_UNIX, '.')
+        );
+    }
+}
+
+let cmd = callbacks.command();
+cmd = cmd.replace('<paths>', pathList.join(' '));
+
+let isPassed = true;
+try {
+    execSync(cmd);
+} catch (e) {
+    isPassed = false;
+}
+
+if (!isPassed) {
+    process.exit(1);
+}
