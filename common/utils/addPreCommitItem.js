b3d092d3d5b55eb961932f06ee78fbfd3c75bb22
OrangeJ <juhaotian@foxmail.com>
2018-09-26 00:53:39 +0800

    fix: #1 - 向pre-commit里增加项时检测是否已存在该项, 若已存在则不再加

--- common/utils/addPreCommitItem.js
+++ common/utils/addPreCommitItem.js
@@ -25,28 +25,33 @@ function addPreCommitItem(json, scriptName) {
         preCommit = null;
     }
 
+    let scripts;
     if (!preCommit) { // no "pre-commit" or "precommit" config now
         json[preCommitKey] = [scriptName];
     } else if (Object.prototype.toString.call(preCommit) === '[object Object]') {
         if (Array.isArray(json[preCommitKey].run)) { // for format of {run: ["a", "b", "c"]}
-            json[preCommitKey].run.push(scriptName);
+            if (!json[preCommitKey].run.includes(scriptName)) {
+                json[preCommitKey].run.push(scriptName);
+            }
         } else if (typeof json[preCommitKey].run === 'string') { // for format of {run: "a, b, c"}
-            json[preCommitKey].run = preCommit.run
-                .split(/[, ]+/)
-                .filter(cmd => !!cmd)
-                .concat(scriptName)
-                .join(', ');
+            scripts = preCommit.run.split(/[, ]+/).filter(cmd => !!cmd);
+
+            if (!scripts.includes(scriptName)) {
+                json[preCommitKey].run = scripts.concat(scriptName).join(', ');
+            }
         } else {
             json[preCommitKey].run = [scriptName];
         }
     } else if (typeof preCommit === 'string') { // for format of "a, b, c"
-        json[preCommitKey] = preCommit
-            .split(/[, ]+/)
-            .filter(cmd => !!cmd)
-            .concat(scriptName)
-            .join(', ');
+        scripts = preCommit.split(/[, ]+/).filter(cmd => !!cmd);
+
+        if (!scripts.includes(scriptName)) {
+            json[preCommitKey] = scripts.concat(scriptName).join(', ');
+        }
     } else if (Array.isArray(preCommit)) { // for format of ["a, b, c"]
-        json[preCommitKey].push(scriptName);
+        if (!preCommit.includes(scriptName)) {
+            json[preCommitKey].push(scriptName);
+        }
     }
 
     return json;
